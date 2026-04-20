712b74aee89b3f6d85146b2df74b020d836bfff7
AlexisRenchon <a.renchon@gmail.com>
2026-04-20 09:18:32 -0700

    updates

--- examples/paramviz/hetero_resp.jl
+++ examples/paramviz/hetero_resp.jl
@@ -7,7 +7,7 @@ function ClimaViz.parameterisation(
     Tₛ,
     θ, # drivers
     ν,
-    α_sx,
+    V_ref_sx,
     Ea_sx,
     kM_sx,
     kM_O₂,
@@ -16,6 +16,7 @@ function ClimaViz.parameterisation(
     Csom, # parameters
     D_liq,
     D_oa,
+    T_ref_sx,
     R_gas,
 ) # constants
     # θ has to be lower than porosity
@@ -25,8 +26,8 @@ function ClimaViz.parameterisation(
     # O₂ availability (Millington-Quirk tortuosity)
     θ_a = max(ν - θ, FT(0))
     O2_avail = D_oa * O₂_a * θ_a^(FT(4 / 3))
-    # DAMM model (Dual Arrhenius Michaelis-Menten)
-    Vmax = α_sx * exp(-Ea_sx / (R_gas * Tₛ))
+    # DAMM model (Dual Arrhenius Michaelis-Menten), centered Arrhenius form
+    Vmax = V_ref_sx * exp(-Ea_sx / R_gas * (FT(1) / Tₛ - FT(1) / T_ref_sx))
     Sx = p_sx * Csom * D_liq * max(θ, FT(0))^3
     MM_sx = Sx / (kM_sx + Sx)
     MM_o2 = O2_avail / (kM_O₂ + O2_avail)
@@ -48,7 +49,7 @@ function Rh_app_f()
     parameters = Parameters(
         (# names
             "Soil porosity, ν (m³ m⁻³)",
-            "Pre-exponential factor, α_sx (kg C m⁻³ s⁻¹)",
+            "Reference respiration rate at T_ref_sx, V_ref_sx (kg C m⁻³ s⁻¹)",
             "Activation energy, Ea_sx (J mol⁻¹)",
             "Michaelis constant for soil, kM_sx (kg C m⁻³)",
             "Michaelis constant for O₂, kM_O₂ (m³ m⁻³)",
@@ -56,19 +57,19 @@ function Rh_app_f()
             "Fraction of soil carbon that is considered soluble, p_sx (dimensionless)",
             "Soil organic C, Csom (kg C m⁻³)",
         ),
-        (# ranges
-            FT.([0.0, 1.0]), # porosity
-            FT.([100e3, 300e3]), # α_sx
-            FT.([50e3, 70e3]), # Ea_sx
-            FT.([1e-10, 0.1]), # kM_sx
-            FT.([1e-10, 0.1]), # kM_O₂
-            FT.([0.005, 0.5]), # O₂_a
-            FT.([0.005, 0.5]), # p_sx
-            FT.([1.0, 10.0]), # Csom
+        (# ranges (centered around toml/default_parameters.toml values)
+            FT.([0.0, 1.0]), # porosity (site-specific)
+            FT.([1e-7, 5e-7]), # V_ref_sx (default: 2.526e-7 kg C m⁻³ s⁻¹)
+            FT.([35e3, 45e3]), # Ea_sx (default: 40000 J mol⁻¹)
+            FT.([0.05, 0.6]), # kM_sx (default: 0.3 kg C m⁻³)
+            FT.([4e-4, 0.04]), # kM_O₂ (default: 0.004 m³ m⁻³)
+            FT.([0.05, 0.4]), # O₂_a (default: 0.2095)
+            FT.([0.002, 0.25]), # p_sx (default: 0.024)
+            FT.([1.0, 10.0]), # Csom (site-specific)
         ),
         (
             (m^3 * m^-3, m^3 * m^-3), # porosity
-            (kg * m^-3 * s^-1, kg * m^-3 * s^-1), # α_sx
+            (kg * m^-3 * s^-1, kg * m^-3 * s^-1), # V_ref_sx
             (J * mol^-1, J * mol^-1), # Ea_sx
             (kg * m^-3, kg * m^-3), # kM_sx
             (m^3 * m^-3, m^3 * m^-3), # kM_O₂
@@ -82,11 +83,13 @@ function Rh_app_f()
         (# names
             "Diffusivity of soil C substrate in liquid (unitless)",
             "Diffusion coefficient of oxygen in air (unitless)",
+            "Reference temperature for centered Arrhenius, T_ref_sx (K)",
             "Universal gas constant (J mol⁻¹ K⁻¹)",
         ),
         (# values
             FT(3.17), # D_liq
             FT(1.67), # D_oa
+            FT(288.15), # T_ref_sx
             R_gas,
         ),
     )
@@ -95,7 +98,7 @@ function Rh_app_f()
 
     output = Output(
         "Rh (mg C m⁻³ s⁻¹)", # name
-        [0, 20 * 1e-6], # range
+        [0, 1 * 1e-6], # range
         (mol * m^-2 * s^-1, μmol * m^-2 * s^-1), # unit from, unit to
     )
 
